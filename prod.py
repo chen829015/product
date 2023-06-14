47f398738ec3eb1fbf94be6f37b7d0aed516981e
ANDY CHEN <chen829015@gmail.com>
2023-06-14 19:34:21 +0800

    new version

--- prod.py
+++ prod.py
@@ -4,9 +4,16 @@ while True:
 	if n == 'q':
 		break
 	pri = input('price:')
-	p = [n ,pri] # p = [n, pri] = p.append(n) p.append(pri)
+	pri = int(pri)
+	#p = [n ,pri] # p = [n, pri] = p.append(n) p.append(pri)
 	prod.append([n ,pri]) # ([m, pri]) = (p) = [n, pri] 
 print(prod)
 
 for i in prod:
 	print(i[0], 'price is', i[1])
+
+
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+	f.write('product, price\n')
+	for i in prod:
+		f.write(i[0] + ',' + str(i[1]) + '\n')
